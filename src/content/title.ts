65120fd99a16620190c02ada12db3d0c73b55dbc
m.m.aal <60576890+THEpErplexed@users.noreply.github.com>
2023-05-11 13:03:34 +0300

    Update title.ts
    eng

--- src/content/title.ts
+++ src/content/title.ts
@@ -2,25 +2,25 @@ import { Sema } from "async-sema";
 import browser from "webextension-polyfill";
 
 /**
- * 求められるままネットワークコネクションを開きまくるとブラウザの動作に支障が出ることと、
- * 閉じたページのタイトルを取得し続けるのは無駄なので、
- * ページ単位で制限を設けます。
+ * If you keep opening network connections as requested, it will interfere with the operation of the browser,
+ * Since it's useless to keep getting the title of the closed page,
+ * Set a limit per page.
  */
 const fetchSema = new Sema(3);
 
 /**
- * ページ内容のタイトルをセマフォの制限付きでbackgroundから取得します。
- * `try`に伴った`let`を書きたくなかったので分けました。
+ * Get page content title from background with semaphore restrictions.
+ * I didn't want to write `let` with `try`, so I split it.
  */
 async function fetchBackground(url: string): Promise<string | undefined> {
   await fetchSema.acquire();
   try {
     const newTitle: unknown = await browser.runtime.sendMessage(url);
-    // 非対応の場合などでタイトルが帰ってこないことがあり、その場合正常に終了します。
+    // The title may not be returned due to unsupported cases, etc., in which case it will end normally.
     if (newTitle == null) {
       return undefined;
     }
-    // タイトルがstringではない場合プログラミングミスなので例外を投げます。
+    // If the title is not a string, throw an exception as it is a programming error.
     if (typeof newTitle !== "string") {
       throw new Error(
         `newTitle !== "string": typeof newTitle is ${typeof newTitle}, newTitle: ${JSON.stringify(
@@ -31,26 +31,26 @@ async function fetchBackground(url: string): Promise<string | undefined> {
     return newTitle;
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error("fetchBackground is error.", err);
+    console. error("fetchBackground is error.", err);
     return undefined;
   } finally {
-    fetchSema.release();
+    fetchSema. release();
   }
 }
 
 /**
- * ページ内容のタイトルをbackgroundから取得して、取得出来たものにタイトルを書き換えます。
- * @param url - 取得対象のページのURL、すぐデシリアライズしてメッセージとして送るのでstring
- * @param link - 検索結果部分のHTML要素
+ * Get the title of the page content from the background and rewrite the title to what you got.
+ * @param url - The URL of the page to get, deserialized immediately and sent as a message string
+ * @param link - HTML element for search results part
  */
 async function replace(url: string, link: Element): Promise<void> {
-  // backgroundから未省略のタイトルを取得します。
+  // Get the full title from the background.
   const newTitle = await fetchBackground(url);
-  // 非対応の場合などでタイトルが帰ってこないことがあり、その場合正常に終了します。
+  // The title may not be returned due to unsupported cases, etc., in which case it will end normally.
   if (newTitle == null) {
     return;
   }
-  // 該当の検索結果からタイトル部分を表示するDOMを取得します。
+  // Get the DOM that displays the title part from the corresponding search results.
   const titleElement = link.querySelector(".LC20lb");
   if (titleElement == null) {
     throw new Error("titleElement is null");
@@ -58,26 +58,26 @@ async function replace(url: string, link: Element): Promise<void> {
   if (!(titleElement instanceof HTMLElement)) {
     throw new Error("titleElement is not HTMLElement");
   }
-  // 省略記号によってタイトルの長さが水増しされていることがあるので、省略記号っぽいものは除去します。
+  // Remove anything that looks like an ellipsis, since the ellipsis can inflate the length of the title.
   const oldTitle = titleElement.textContent?.replace("...", "") || "";
   if (newTitle.length < oldTitle.length) {
-    // 古いタイトルの方が長い場合取得失敗の可能性が高いので、置き換えを行いません。
+    // If the old title is longer, there is a high possibility of acquisition failure, so we do not replace it.
     return;
   }
-  // リンクが異常に長いことがあります。
-  // 間違えてtitleタグが閉じられてないとか、
-  // HTMLじゃないものをHTMLとして認識してしまっていると言ったケースです。
-  // その場合置き換えを行いません。
-  // 異常な長さを検出することが目的なので日本語と英語の長さの違いなどは考慮しません。
+  // Links can be unusually long.
+  // If the title tag is not closed by mistake,
+  // This is a case of recognizing something that is not HTML as HTML.
+  // In that case, do not replace.
+  // Since the purpose is to detect abnormal lengths, differences in Japanese and English lengths are not considered.
   if (newTitle.length > 500) {
     return;
   }
-  // 改行コードを反映させたいのでtextContentではなくinnerTextを使って代入する。
-  titleElement.innerText = newTitle;
+  // Since we want to reflect the line feed code, we use innerText instead of textContent for assignment.
+  titleElement. innerText = newTitle;
 }
 
 /**
- * 要素を置き換える関数を呼び出します。
+ * Call a function that replaces an element.
  */
 async function replaceLinkTitle(link: Element): Promise<void> {
   try {
@@ -88,13 +88,13 @@ async function replaceLinkTitle(link: Element): Promise<void> {
     return await replace(href, link);
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error("replaceLinkTitle is error.", err, link);
+    console. error("replaceLinkTitle is error.", err, link);
     return undefined;
   }
 }
 
 /**
- * 複数の要素を順不同で置き換えます。
+ * Replace multiple elements in any order.
  */
 export async function replaceLinkTitles(links: Element[]): Promise<void[]> {
   return Promise.all(links.map((link) => replaceLinkTitle(link)));
